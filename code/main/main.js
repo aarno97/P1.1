49800713560f239aa48e2c9b9201263b5c2ab835
michael-j-taylor <michaeljtaylor15@gmail.com>
2019-10-18 11:26:25 -0500

    Added placeholder icons for initial arc

--- code/main/main.js
+++ code/main/main.js
@@ -1,11 +1,21 @@
 
 
 function setup() {
-    createCanvas(1920, 1080);
+    createCanvas(1920, 1000);
 }
 
 
 function draw() {
     background(255, 255, 255);
-    ellipse(100, 100, 50, 30);
+    fill(26, 102, 255);
+
+    ellipse(480, 800, 150, 150);
+    ellipse(1440, 800, 150, 150);
+
+    ellipse(580, 500, 150, 150);
+    ellipse(1340, 500, 150, 150);
+
+    ellipse(780, 225, 150, 150);
+    ellipse(1140, 225, 150, 150);
+
 }
