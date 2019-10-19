dedf78151edd81e58560bf8d0ba985757cd36d9a
michael-j-taylor <michaeljtaylor15@gmail.com>
2019-10-19 12:43:02 -0500

    Implemented video background functionality
    Added full p5.js library file and edited main.js to allow mirror simulation

--- code/main/main.js
+++ code/main/main.js
@@ -2,34 +2,46 @@
 var state;  //current state of mirror ("inactive", "home", or name of application in fullscreen);
 var video;
 
+let capture;
+
 /**
  * Class containing data and methods required to draw fullscreen application
  */
 class App {
-    constructor(name) {
+    constructor(name, icon) {
         this.name = name;
+        this.icon = icon;
     };
-    draw_app() {};
+    draw_app_icon() {};
+    draw_app_fullscreen() {};
 }
 
 
 function setup() {
     createCanvas(1000, 500);
-    video = createCapture(VIDEO);
-    video.size(320, 240);
+    capture = createCapture(VIDEO);
+    capture.size(1000, 500);
+    capture.hide();  //prevents duplicate feed
 }
 
 
 function draw() {
-    background(255, 255, 255);
-    image(video, 0, 0);
-    fill(26, 102, 255);
-
-    rect(75, 150, 925, 775);
+    //draw webcam feed as background
+    image(capture, 0, 0, 1000, 500);
+    draw_home();
 }
 
 
-function draw_clock(x, y) {};
+function draw_clock(x, y) {}
 
 
-function draw_home() {};
+function draw_home() {
+    fill(26, 102, 255);
+
+    //rect(75, 75, 850, 350);
+}
+
+
+function draw_initial() {
+    ellipse(50,50,50,50);
+}
