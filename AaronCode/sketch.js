3b03c6a3a0b1f5ea3f69822877aae4d08e7a0066
Aaron Arnold <pape.aaron97@icloud.com>
2019-10-23 20:02:44 -0500

    Adding Google Calendar API
    Pulling directly from Google

--- AaronCode/sketch.js
+++ AaronCode/sketch.js
@@ -17,33 +17,4 @@ function setup() {
 
 function draw() {
     ellipse(100, 100, 250, 250);
-    document.addEventListener('DOMContentLoaded', function() {
-        let calendarEl = document.getElementById('calendar');
-
-        let calendar = new FullCalendar.Calendar(calendarEl, {
-            height: 500,
-            plugins: [ 'dayGrid', 'list', 'googleCalendar' ],
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,listYear'
-            },
-
-            displayEventTime: false, // don't show the time column in list view
-
-            //Pulling APIKey until launch
-            googleCalendarApiKey: '',
-
-            //allows us to pull data from multiple calendars: regular calendar and US Holidays
-            eventSources: [
-                {
-                    googleCalendarId: 'mfjsnor8bvj6c4a0vshb24g7sk@group.calendar.google.com'
-                },
-                {
-                    googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com',
-                }
-            ],
-        });
-        calendar.render();
-    });
 }
