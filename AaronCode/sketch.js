9dbe7823f70e50c3873009bd929d38446a26c3a5
Aaron Arnold <pape.aaron97@icloud.com>
2019-10-26 14:32:02 -0500

    Updating Calendar

--- AaronCode/sketch.js
+++ AaronCode/sketch.js
@@ -1,10 +1,13 @@
+var Calendar;
+
 function setup() {
     createCanvas(1000, 500);
     background(228, 229, 240);
+    Calendar = loadImage("Calendar.png");
 
 }
 
 function draw() {
     rect(75, 75, 850, 350);
-
+    image(Calendar, 75,75, 850,350);
 }
