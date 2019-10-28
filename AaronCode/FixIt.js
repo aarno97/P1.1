ce4afc98bd4265fa58565213606e8827767bf811
Aaron Arnold <pape.aaron97@icloud.com>
2019-10-28 15:43:33 -0500

    Added working calendar, working on Apple Music widget

--- AaronCode/FixIt.js
+++ AaronCode/FixIt.js
@@ -10,6 +10,8 @@ let MovButton;
 let SleeButton;
 let StanButton;
 let SteButton;
+let calButton;
+let musButton;
 
 function setup() {
     createCanvas(1000,500);
@@ -24,35 +26,65 @@ function setup() {
     ExerButton.position(75,50);
     ExerButton.mousePressed(ExerciseButton);
     MovButton = createButton('Move Results');
-    MovButton.position(300, 50);
+    MovButton.position(275, 50);
     MovButton.mousePressed(MoveButton);
     SleeButton = createButton('Sleep Results');
-    SleeButton.position(425, 50);
+    SleeButton.position(450, 50);
     SleeButton.mousePressed(SleepButton);
     StanButton = createButton('Stand Results');
-    StanButton.position(600, 50);
+    StanButton.position(650, 50);
     StanButton.mousePressed(StandButton);
     SteButton = createButton('Step Results');
     SteButton.position(850, 50);
-    SteButton.mousePressed(StepButton)
+    SteButton.mousePressed(StepButton);
+    calButton = createButton('Calendar');
+    calButton.position(75, 100);
+    calButton.mousePressed(CalendarButton);
+    musButton = createButton('Music');
+    musButton.position( 275, 75);
+    musButton.mousePressed(MusicButton);
 }
 
 function draw() {
     background(228, 229, 240);
     let i = State;
     if(i === 'Calendar') {
-        image(Calendar, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).position(75, 75);
+        select('#calendar', HTMLElement).show();
+        select('#music', HTMLElement).hide();
+    } else if(i === 'Music') {
+        select('#music', HTMLElement).position(75, 75);
+        select('#music', HTMLElement).show();
+        select('#calendar', HTMLElement).hide();
     } else if(i === 'Exercise') {
         image(Exercise, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).hide();
+        select('#music', HTMLElement).hide();
     } else if(i === 'Move') {
         image(Move, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).hide();
+        select('#music', HTMLElement).hide();
     } else if(i === 'Sleep') {
         image(Sleep, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).hide();
+        select('#music', HTMLElement).hide();
     } else if(i === 'Stand') {
         image(Stand, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).hide();
+        select('#music', HTMLElement).hide();
     } else if(i === 'Step') {
         image(Step, 75, 75, 850, 350);
+        select('#calendar', HTMLElement).hide();
+        select('#music', HTMLElement).hide();
     }
+}
+
+function musicButton() {
+    State = 'Music';
+}
+
+function CalendarButton() {
+    State = 'Calendar';
 }
 
 function ExerciseButton() {
