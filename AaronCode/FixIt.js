7a464251bf031ba22d9970eb42707ce42be99e21
Aaron Arnold <pape.aaron97@icloud.com>
2019-10-28 17:28:58 -0500

    Integrated Google Maps, Calendar, Apple Music, working on code refactoring

--- AaronCode/FixIt.js
+++ AaronCode/FixIt.js
@@ -4,7 +4,7 @@ let Move;
 let Sleep;
 let Stand;
 let Step;
-let State = 'Calendar';
+let State = 'Map';
 let ExerButton;
 let MovButton;
 let SleeButton;
@@ -13,6 +13,7 @@ let SteButton;
 let calButton;
 let musButton;
 let mapButton;
+let healButton;
 
 function setup() {
     createCanvas(1000,500);
@@ -23,6 +24,45 @@ function setup() {
     Sleep = loadImage('Sleep Goal.png');
     Stand = loadImage('Stand Goal.png');
     Step = loadImage('Step Goal.png');
+    createHealth();
+    healButton = createButton('Health');
+    healButton.position(650, 75);
+
+    calButton = createButton('Calendar');
+    calButton.position(75, 75);
+    calButton.mousePressed(CalendarButton);
+    musButton = createButton('Music');
+    musButton.position( 275, 75);
+    musButton.mousePressed(MusicButton);
+    mapButton = createButton('Map');
+    mapButton.position(450, 75);
+    mapButton.mousePressed(MapResults);
+}
+
+function draw() {
+    background(228, 229, 240);
+    let i = State;
+    if(i === 'Map') {
+        hideHealth();
+        hideAll();
+        select('#map', HTMLElement).position(75,75);
+        select('#map', HTMLElement).show();
+    } else if(i === 'Music') {
+        hideHealth();
+        hideAll();
+        select('#music', HTMLElement).position(75, 75);
+        select('#music', HTMLElement).show();
+    } else if(i === 'Calendar') {
+        hideHealth();
+        hideAll();
+        select('#calendar', HTMLElement).position(75,75);
+        select('#calendar', HTMLElement).show();
+    } else if(i === 'Health') {
+
+    }
+}
+
+function createHealth() {
     ExerButton = createButton('Exercise Results');
     ExerButton.position(75,50);
     ExerButton.mousePressed(ExerciseButton);
@@ -38,62 +78,28 @@ function setup() {
     SteButton = createButton('Step Results');
     SteButton.position(850, 50);
     SteButton.mousePressed(StepButton);
-    calButton = createButton('Calendar');
-    calButton.position(75, 100);
-    calButton.mousePressed(CalendarButton);
-    musButton = createButton('Music');
-    musButton.position( 275, 75);
-    musButton.mousePressed(MusicButton);
-    // mapButton = createButton('Map');
-    // mapButton.position(450, 75);
-    // mapButton.mousePressed(MapResults);
 }
 
-function draw() {
-    background(228, 229, 240);
-    let i = State;
-    if(i === 'Calendar') {
-        select('#calendar', HTMLElement).position(75, 75);
-        select('#calendar', HTMLElement).show();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    } else if(i === 'Music') {
-        select('#music', HTMLElement).position(75, 75);
-        select('#music', HTMLElement).show();
-        select('#calendar', HTMLElement).hide();
-        select('map', HTMLElement).hide();
-        // } else if(i === 'Map') {
-        //     select('map', HTMLElement).position(75,75);
-        //     select('map', HTMLElement).show();
-        //     select('#calendar', HTMLElement).hide();
-        //     select('#music', HTMLElement).hide();
-        // }
-    } else if(i === 'Exercise') {
-        image(Exercise, 75, 75, 850, 350);
-        select('#calendar', HTMLElement).hide();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    } else if(i === 'Move') {
-        image(Move, 75, 75, 850, 350);
-        select('#calendar', HTMLElement).hide();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    } else if(i === 'Sleep') {
-        image(Sleep, 75, 75, 850, 350);
-        select('#calendar', HTMLElement).hide();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    } else if(i === 'Stand') {
-        image(Stand, 75, 75, 850, 350);
-        select('#calendar', HTMLElement).hide();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    } else if(i === 'Step') {
-        image(Step, 75, 75, 850, 350);
-        select('#calendar', HTMLElement).hide();
-        select('#music', HTMLElement).hide();
-        // select('map', HTMLElement).hide();
-    }
+function showHealth() {
+    ExerButton.show();
+    MovButton.show();
+    SleeButton.show();
+    SteButton.show();
+    StanButton.show();
+}
+
+function hideHealth() {
+    ExerButton.hide();
+    MovButton.hide();
+    SleeButton.hide();
+    SteButton.hide();
+    StanButton.hide();
+}
+
+function hideAll() {
+    select('#map', HTMLElement).hide();
+    select('#calendar', HTMLElement).hide();
+    select('#music', HTMLElement).hide();
 }
 
 function MapResults() {
