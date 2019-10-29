063222dbfa9538d6f2b22f22c05dd61bda4b6208
Aaron Arnold <pape.aaron97@icloud.com>
2019-10-28 21:37:21 -0500

    Added commented code to make background picture

--- AaronCode/FixIt.js
+++ AaronCode/FixIt.js
@@ -1,10 +1,13 @@
 let State = 'Calendar';
 let HealthState = 'Exercise';
-let Calendar, Exercise, Move, Sleep, Stand, Step, ExerButton, MovButton, SleeButton, StanButton, SteButton, calButton, musButton, mapButton, healButton;
+let Calendar, Exercise, Move, Sleep, Stand, Step, ExerButton, MovButton, SleeButton, StanButton, SteButton, calButton, musButton, mapButton, healButton, backButton;
+// let backImg;
 
 function setup() {
     createCanvas(1000,500);
     background(228,229,240);
+    // backImg = createImg('https://source.unsplash.com/random/1000x500');
+    // backImg.hide();
     Calendar = loadImage('Calendar.png');
     Exercise = loadImage('Exercise Goal.png');
     Move = loadImage('Move Goal.png');
@@ -17,6 +20,11 @@ function setup() {
 
 function draw() {
     background(228, 229, 240);
+    // tint(255, 127);
+    // image(backImg, 0, 0, 1000, 500);
+    // tint(255, 255);
+    // fill(228, 229, 240);
+    // rect(50, 50, 875, 375);
     let i = State;
     if(i === 'Map') {
         hideHealth();
@@ -53,17 +61,21 @@ function draw() {
 
 function buttonMaker() {
     healButton = createButton('Health');
-    healButton.position(650, 75);
+    healButton.position(75, 50);
     healButton.mousePressed(HealthButton);
     calButton = createButton('Calendar');
-    calButton.position(75, 75);
+    calButton.position(325, 50);
     calButton.mousePressed(CalendarButton);
     musButton = createButton('Music');
-    musButton.position( 275, 75);
+    musButton.position( 600, 50);
     musButton.mousePressed(MusicButton);
     mapButton = createButton('Map');
-    mapButton.position(450, 75);
+    mapButton.position(885, 50);
     mapButton.mousePressed(MapResults);
+    backButton = createButton('<- Back');
+    backButton.position(75, 50);
+    backButton.hide();
+    backButton.mousePressed(HealthButton);
 }
 
 function showOther() {
@@ -80,19 +92,19 @@ function hideOther() {
 
 function createHealth() {
     ExerButton = createButton('Exercise Results');
-    ExerButton.position(75,50);
+    ExerButton.position(200,50);
     ExerButton.mousePressed(ExerciseButton);
     MovButton = createButton('Move Results');
-    MovButton.position(275, 50);
+    MovButton.position(325, 50);
     MovButton.mousePressed(MoveButton);
     SleeButton = createButton('Sleep Results');
     SleeButton.position(450, 50);
     SleeButton.mousePressed(SleepButton);
     StanButton = createButton('Stand Results');
-    StanButton.position(650, 50);
+    StanButton.position(575, 50);
     StanButton.mousePressed(StandButton);
     SteButton = createButton('Step Results');
-    SteButton.position(850, 50);
+    SteButton.position(700, 50);
     SteButton.mousePressed(StepButton);
 }
 
@@ -102,6 +114,8 @@ function showHealth() {
     SleeButton.show();
     SteButton.show();
     StanButton.show();
+    backButton.show();
+    healButton.hide();
 }
 
 function hideHealth() {
@@ -110,6 +124,8 @@ function hideHealth() {
     SleeButton.hide();
     SteButton.hide();
     StanButton.hide();
+    backButton.hide();
+    healButton.show();
 }
 
 function hideAll() {
@@ -121,9 +137,12 @@ function hideAll() {
 function HealthButton() {
     if(State === 'Health') {
         State = 'Map';
+        backButton.show();
         showOther();
     } else {
         State = 'Health';
+        healButton.show();
+        backButton.hide();
     }
 }
 
